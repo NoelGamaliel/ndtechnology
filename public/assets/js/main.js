223f3d5c777ec8ec1c34f2876c1fe044bc5c1b8b
DIAMONA <64137394+NoelGamaliel@users.noreply.github.com>
2025-06-07 20:46:47 -0600

    Ajout the index js for the reacting and something update

---
+++ public/assets/js/main.js
@@ -0,0 +1,7 @@
+const mousemove = document.getElementById('mousemove');
+
+window.addEventListener("mousemove", (e) => {
+    mousemove.style.left = e.pageX + "px";
+    mousemove.style.top = e.pageY + "px";   
+})
+
